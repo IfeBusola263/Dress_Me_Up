973704c3af17836d0aadd6f65a0d9270e4a64305
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-11-28 13:11:08 +0000

    Password hash was well sorted with the correct error messages

--- api/v1/views/user.py
+++ api/v1/views/user.py
@@ -5,6 +5,7 @@ from api.v1.views import app_views
 from models.user import User
 from flask import jsonify, request, render_template
 from models import storage
+import hashlib
 
 
 @app_views.route('/users/sign_up',
@@ -17,7 +18,7 @@ def sign_up():
     if "name" not in detail:
         return jsonify("name cannot be empty"), 400
 
-    if not "password" in detail:
+    if "password" not in detail:
         return jsonify("password cannot be empty"), 400
 
     new_user = User(**detail)
@@ -25,34 +26,34 @@ def sign_up():
 
     return jsonify("Peng! You have successfully registered"), 201
 
+
 @app_views.route('/users/login',
                  methods=["POST"],
                  strict_slashes=False)
 def log_in(username=None, email=None, password=None):
     """Defines user login utility"""
     detail = request.get_json()
-    print(detail)
 
-    if not "name" in detail:
+    if "name" not in detail:
         return jsonify("Username cannot be empty"), 400
 
-    if not "password" in detail:
+    if "password" not in detail:
         return jsonify("Password is required"), 400
 
     for value in storage.all(User).values():
-        #print(value)
-        dtl_name = detail["name"]
 
-        val_name = value.make_json().get("name")
-        dtl_pwd = detail["password"]
-        val_pwd = value.make_json().get("password")
-        detail_dict = {
-                dtl_name: val_name,
-                dtl_pwd: val_pwd
-                }
-        print(detail_dict)
-        if detail["name"] == value.make_json().get("name") \
-                and detail["password"] == value.make_json().get("password"):
-                    return "<h1>Hello World!</h1>"
-                    #return render_template("login.html")
+        # Confirm user name
+        if detail["name"] == value.make_json().get("name"):
+
+            # Verify Password
+            passwd = hashlib.md5(detail['password'].encode('utf-8'))
+            passwd = passwd.hexdigest()
+            if passwd == value.make_json().get("password"):
+                # return "<h1>Hello World!</h1>"
+                return render_template("login.html", name=detail['name'])
+
+            # Wrong password details
+            return jsonify('Wrong Password, you can try again')
+
+    # User not found in the database
     return jsonify("User not found")
